ca80d520e03b1f753294470066e09956d6c4772f
s4timuen <tim.muenzinger@yahoo.de>
2022-05-23 11:12:43 +0200

    Spring Boot 2 Setup.

---
+++ src/main/java/com/muenzinger/wta_api/WorkoutTrackerAnalyzerApiApplication.java
@@ -0,0 +1,13 @@
+package com.muenzinger.wta_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoutTrackerAnalyzerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkoutTrackerAnalyzerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/muenzinger/wta_api/WorkoutTrackerAnalyzerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muenzinger.wta_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutTrackerAnalyzerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
